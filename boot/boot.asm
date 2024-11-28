932ce30e3da73cad7a02f090744eefbaa838914e
perryMagnus <130815910+perryMagnus@users.noreply.github.com>
2024-11-28 17:49:31 +0100

    managed to switch into 32 bits, great success

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,41 +2,64 @@
 [ORG 0x7C00]   ; classic place of the bootloader
 
 _start:
-    mov ax, 0x00
-    mov bx, 0x00
-    mov cx, 0x00
-    mov dx, 0x00
-
-    ; turning the A20 line 
-    ; in order to switch to Protected Mode (32 bits)
-
-    in al, 0x92    ; download the state of 0x92 I/O port
-    or al, 2       ; manipulate it
-    out 0x92, al   ; send it back, changing the behaviour of the I/O port
-
-    mov si, message
-    call print
-
-    ; Next goal:
-    ; load the GDT, and design it. 
-    ; Try to do it in your own way of design, exactly like you want it.
-    ; Also, gather information about GDTs in general.
-    ; Bit by bit. (almost like brick by brick lol)
-
-print:
-    lodsb          ; Moves SI into AL, lodsb = LOaD String Byte
-    or al, al      ; Checks if al is null-terminator (0x00)
-    jz print_finished   ; "jump if zero"
-
-    mov ah, 0x0E   ; BIOS's teletype function
+    mov ah, 0x00
+    mov al, 0x03
     int 0x10
-    jmp print
 
-print_finished:
-    ret    ; return
+    jmp protModeSetup
+    
+    
+protModeSetup:
+    cli                     ; disable interrupts
+
+    in al, 0x92             ; download the state of I/O port 0x92
+    or al, 2                ; manipulate it
+    out 0x92, al            ; send it back, enabling A20 line
+    
+    xor ax, ax              ; prepare to initialize ds
+    mov ds, ax              ; initialize ds, lgdt uses ds as it's segment
+
+    lgdt [GDT_DESC]         ; load the GDT
+
+    mov eax, 0x11           ; manipulate eax (eax is zero'ed)
+
+    ; mov eax to Control Register 0
+    ; paging disabled, protection bit 0 enabled, bit 4, the extension type is alwawys 1
+
+    mov cr0, eax            
+
+    jmp GDT_kernelCodeSegment-GDT:protmode      ; jump to Protected Mode part 
+
+    
+[BITS 32]
+
+protmode:
+    hlt
+
+    ; Next goals:
+    ; Set up GDT for Long Mode
+    ; Print out something in 32 bits
+    ; Make notes about how do you switch to 32 bits
+
+align 4
+GDT_DESC:
+    dw GDT_END - GDT - 1       ; gdt_start and _end are memory addresses. This allows us to measure the size of GDT
+    dd GDT                     ; dd (define doubleworld) "enables" the GDT in 32-bit mode (dq is for 64 bits, "define quadword")
+
+align 8
+GDT:
+    GDT_NULL: dq 0x0000000000000000                         ; Null descriptor
+    GDT_kernelCodeSegment: dq 0x00CF9A000000FFFF            ; Kernel Code Segment
+    GDT_kernelDataSegment: dq 0x00CF92000000FFFF            ; Kernel Data Segment
+    GDT_userCodeSegment: dq 0x00CFFA000000FFFF              ; User Code Segment
+    GDT_userDataSegment: dq 0x00CF92000000FFFF              ; User Data Segment
+    GDT_TSM: dq 0x0000000000000000                          ; Task State Managent
+GDT_END:
 
 
-message db "Hello, world! Bootloader's here B).", 0   ; string var
+
+message db "You're in!", 0   ; string var
+messageProt db "ProtMode!", 0
 
 times 510 - ($ - $$) db 0   ; fill 510 bits with 0's
 dw 0xAA55                   ; bootloader's signature
